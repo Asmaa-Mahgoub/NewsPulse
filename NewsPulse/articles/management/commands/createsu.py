e82763654d01e42c76d267f43b90c27dd81524a1
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-10-02 22:09:49 +0300

    Add render.yaml with post-deploy superuser creation

---
+++ NewsPulse/articles/management/commands/createsu.py
@@ -0,0 +1,20 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Create a superuser if none exist"
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                email=os.environ["DJANGO_SUPERUSER_EMAIL"],
+                username=os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin"),
+                password=os.environ["DJANGO_SUPERUSER_PASSWORD"],
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser created!"))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists."))
+
+
